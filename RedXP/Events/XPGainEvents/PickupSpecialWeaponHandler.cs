0b8fa6a0a9303843ac0fdfa957d4f444df84c763
git4rker <unicod3r.ivan@gmail.com>
2025-07-25 14:25:17 +0300

    XPGainEvents: add COM-45 and A7 to the special weapon list

--- RedXP/Events/XPGainEvents/PickupSpecialWeaponHandler.cs
+++ RedXP/Events/XPGainEvents/PickupSpecialWeaponHandler.cs
@@ -10,9 +10,11 @@ public class PickupSpecialWeaponHandler : CustomEventsHandler {
   private static Translations translations => RedXP.Instance.Translations;
 
   private Dictionary<ItemType, List<Player>> rewardsClaimed = new() {
-    { ItemType.MicroHID, new List<Player>() },
-    { ItemType.ParticleDisruptor, new List<Player>() },
-    { ItemType.Jailbird, new List<Player>() }
+    { ItemType.MicroHID, new() },
+    { ItemType.ParticleDisruptor, new() },
+    { ItemType.Jailbird, new() },
+    { ItemType.GunCom45, new() },
+    { ItemType.GunA7, new() }
   };
 
   public override void OnPlayerPickedUpItem(PlayerPickedUpItemEventArgs ev) {
